f1054852ceceda272a01a18c40119f30f2a9f2f8
Mohan <1063022109@qq.com>
2018-09-26 19:28:24 +0800

    首页标题栏

--- src/pages/msite/index.vue
+++ src/pages/msite/index.vue
@@ -2,11 +2,21 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-19 21:07:57 
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-09-24 19:24:48
+ * @Last Modified time: 2018-09-26 19:23:41
  */
 <template>
-  <div class="home">
-    <span>{{ curLocalName }}</span>
+  <div class="msite">
+    <!-- <span></span> -->
+    <header>
+      <div class="msite-location">
+        <img src="../../assets/svg/icon-location.svg" alt="定位">
+        <span class="msg">{{ curLocalName }}</span>
+      </div>
+      <router-link tag="div" class="msite-search" to="/search">
+        <img src="../../assets/svg/icon-search.svg" alt="搜索">
+        <span>搜索饿了么商家、商品名称</span>
+      </router-link>
+    </header>
     <footer-v></footer-v>
   </div>
 </template>
@@ -48,19 +58,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+@import '@/assets/styles/mixin.scss';
+header{
+  background-image: linear-gradient(90deg,#0af,#0085ff);
+  padding: .3rem .24rem .2rem .24rem;
+  .msite-location{
+    @include fj(flex-start ,center);
+    @include sc(.30rem , #ffffff);
+    font-weight: bold;
+  }
+  .msite-search {
+    @include fj(center ,center);
+    @include sc(.28rem ,#999);
+    background-color: #ffffff;
+    margin-top: .2rem;
+    padding: .2rem .2rem;
+    font-family: simsun;
+    text-align: center;
+    border-radius: .05rem;
+  }
 }
 </style>
