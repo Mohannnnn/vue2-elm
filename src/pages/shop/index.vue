3f2638e76b01004ca1633138195877e08cc09a1e
Mohan <1063022109@qq.com>
2018-10-30 20:08:01 +0800

    shop商家

---
+++ src/pages/shop/index.vue
@@ -0,0 +1,26 @@
+/*
+ * @Author: wuhan  [https://github.com/Mohannnnn] 
+ * @Date: 2018-10-30 20:05:31 
+ * @Last Modified by:   wuhan 
+ * @Last Modified time: 2018-10-30 20:05:31 
+ */
+<template>
+   <div></div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        };
+    },
+    components: {},
+    computed: {},
+    watch: {},
+    methods: {},
+    mounted() {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
