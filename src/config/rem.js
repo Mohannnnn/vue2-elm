4b3fc526e8944cbd2c0b2f78a070c434463bbcb5
Mohan <1063022109@qq.com>
2018-11-06 10:08:27 +0800

    修改样式

--- src/config/rem.js
+++ src/config/rem.js
@@ -2,7 +2,7 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-19 21:16:07 
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-09-20 17:57:56
+ * @Last Modified time: 2018-11-05 14:09:38
  */
 (function(doc, win) {
     var docEl = doc.documentElement,
@@ -10,6 +10,9 @@
         recalc = function() {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
+            if (clientWidth > 769) {
+                clientWidth = 750;
+            }
             docEl.style.fontSize = clientWidth / 7.5 + 'px';
         };
     if (!doc.addEventListener) return;
