f8b514ffb7bf7ae67bdfa3202e40f1c586c34080
Mohan <1063022109@qq.com>
2018-11-23 23:13:24 +0800

    Main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 /*
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-11-20 21:07:21 
- * @Last Modified by:   wuhan 
- * @Last Modified time: 2018-11-20 21:07:21 
+ * @Last Modified by: wuhan
+ * @Last Modified time: 2018-11-23 23:13:05
  */
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
 import './config/rem'
-// import Mint from 'mint-ui';
-// import 'mint-ui/lib/style.css'
 
 // Vue.use(Mint);
 Vue.config.productionTip = false
