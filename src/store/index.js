d026639a4b310d8b46e25da213c4e60ee6f9c5d8
Mohan <1063022109@qq.com>
2018-11-25 18:37:44 +0800

    store

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-19 21:10:22 
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-11-20 20:58:37
+ * @Last Modified time: 2018-11-25 18:37:32
  */
 import Vue from 'vue'
 import Vuex from 'vuex';
