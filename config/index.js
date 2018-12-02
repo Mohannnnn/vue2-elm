8400c18802b4ed1553c0a445c00e94e082ac9457
Mohan <1063022109@qq.com>
2018-12-02 14:54:17 +0800

    修改build

--- config/index.js
+++ config/index.js
@@ -63,10 +63,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/elm/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist/elm'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
