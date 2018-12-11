ea85fd969ddb252c926e9e2ba9c5977b80b3249c
Mohan <1063022109@qq.com>
2018-12-11 19:39:09 +0800

    actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,9 @@
 /*
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-19 21:10:10 
- * @Last Modified by:   wuhan 
- * @Last Modified time: 2018-09-19 21:10:10 
+ * @Last Modified by: wuhan
+ * @Last Modified time: 2018-12-11 19:38:55
  */
-
 const actions = {
     //设置经纬度
     setItude({commit} , value) {
